29babc50e82c4b300d8a41a07ad854ae5c2c4845
Alan <alan.tutoria.ti@gmail.com>
2022-08-15 00:32:49 -0300

    metodo de exclusao do usuario Adicionado numero de paginas e autor. Classe livroUtil classe UsuarioUtil

--- src/biblioteca/Livro.java
+++ src/biblioteca/Livro.java
@@ -3,6 +3,7 @@ package biblioteca;
 public class Livro {
 	
 	private String titulo;
+<<<<<<< Upstream, based on 82030e5fe4ba816b2c6a0c3c5541df7b5fcdeafa
 	
 	public void emprestar() {
 	}
@@ -10,3 +11,8 @@ public class Livro {
 	public void devolver() {
 	}
 }
+=======
+	private String numPaginas;
+	private String autor;
+}
+>>>>>>> 9cadf69 Adicionado numero de paginas e autor.
